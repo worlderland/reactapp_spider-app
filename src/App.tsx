db254310dd15cca9d0bbcf45935b2b4a8d8267ea
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2021-07-16 17:10:00 +0800

    Completed the "building what we started" chapter. Started "Almost there" chapter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import TextInputForm from './components/TextInputForm/TextInputForm';
+
+import React, { useState } from 'react';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import PlayContainer from './containers/PlayContainer';
+import StartContainer from './containers/StartContainer';
 
 function App() {
-  return (
-    <div className="App container mx-auto p-5">
-      <h1 className="text-5xl">Spider</h1>
-      <div className="py-10">
-      <TextInputForm
-					onSubmit={(value: string) => {
-						alert(value);
-					}}
-				/>
-      </div>
-    </div>
-  );
+	const history = useHistory();
+
+	const [text, setText] = useState('');
+
+	const handleSubmit = (value: string) => {
+		setText(value.toUpperCase());
+		history.push('/play');
+	};
+
+	return (
+		<div className="App container mx-auto">
+			<h1 className="text-5xl">Spider</h1>
+			<div className="my-8">
+				<Switch>
+					<Route path="/start">
+						<StartContainer onSubmit={handleSubmit} />
+					</Route>
+					<Route path="/play">
+						<PlayContainer text={text} />
+					</Route>
+					<Redirect path="*" to="/start" />
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/containers/PlayContainer.tsx
@@ -0,0 +1,36 @@
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import LetterButtons from '../components/LetterButtons/LetterButtons';
+import MaskedText from '../components/MaskedText/MaskedText';
+
+type PlayContainerProps = {
+	text?: string;
+};
+
+function PlayContainer({ text }: PlayContainerProps) {
+	const [usedLetters, setUsedLetters] = useState<string[]>([]);
+
+	const handleClick = (letter: string) => {
+		setUsedLetters(previous => [...previous, letter]);
+	};
+
+	return (
+		<>
+			<Link
+				className="text-blue-600 underline hover:text-blue-700"
+				to="/start"
+			>
+				Start Over
+			</Link>
+			<MaskedText text={text} usedLetters={usedLetters} />
+			<LetterButtons
+				onClick={handleClick}
+				text={text}
+				usedLetters={usedLetters}
+			/>
+		</>
+	);
+}
+
+export default PlayContainer;
